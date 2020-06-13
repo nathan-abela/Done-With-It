eaedfa8763063a4dae30b7f3b6d149aa397e66da
Nathan Abela <nathanabela7@gmail.com>
2020-06-14 00:19:35 +0200

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// View -> UIView
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<Text>Hello React Native!</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
