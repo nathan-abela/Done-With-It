8b05b0ae3420794dbe3bb9d2c2e21d3a571942d1
Nathan Abela <nathanabela7@gmail.com>
2020-06-14 20:58:23 +0200

    Add View Image Screen

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from '../config/colors';
+
+function ViewImageScreen(props) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.closeIcon}>
+                <MaterialCommunityIcons 
+                    name="close"
+                    color="white"
+                    size={35} 
+                />
+            </View>
+
+            <View style={styles.deleteIcon}>
+                <MaterialCommunityIcons
+                    name="trash-can-outline"
+                    color="white"
+                    size={35}
+                />
+            </View>
+
+            <Image 
+                resizeMode="contain"
+                style={styles.image}
+                source={require('../assets/chair.jpg')}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    closeIcon: {
+        position: "absolute",
+        top: 40,
+        left: 30,
+    },
+    container: {
+        backgroundColor: colors.black,
+        flex: 1,
+    },
+    deleteIcon: {
+        position: "absolute",
+        top: 40,
+        right: 30,
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+});
+
+export default ViewImageScreen;
