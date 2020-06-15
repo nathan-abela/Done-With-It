277de82ca08fab3dd7415ce611bdd1c8cc4656ca
Nathan Abela <nathanabela7@gmail.com>
2020-06-16 01:31:34 +0200

    Add List Item Separator Component

---
+++ app/components/ListItemSeparator.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+import colors from "../config/colors";
+
+function ListItemSeparator() {
+    return <View style={styles.separator} />;
+}
+
+const styles = StyleSheet.create({
+    separator: {
+        width: '100%',
+        height: 1,
+        backgroundColor: colors.light,
+    }
+});
+
+export default ListItemSeparator;
