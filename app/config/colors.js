2d32244192f36ce7ea95b4499738b709882181db
Nathan Abela <nathanabela7@gmail.com>
2020-06-16 01:32:53 +0200

    Add Messages Screen

--- app/config/colors.js
+++ app/config/colors.js
@@ -4,4 +4,6 @@ export default {
     black: "#000",
     white: "#fff",
     medium: "#6e6969",
+    light: "#f8f4f4",
+    danger: "#ff5252"
 }
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+
+const initialMessages = [
+    {
+        id: 1,
+        title: 'T1',
+        description: 'D1',
+        image: require('../assets/user.jpg'),
+    },
+    {
+        id: 2,
+        title: 'T2',
+        description: 'D2',
+        image: require('../assets/user.jpg'),
+    }
+]
+
+function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const handleDelete = (message) => {
+        // Deletes the message from messages
+        setMessages(messages.filter((m) => m.id !== message.id));
+    };
+
+    return (
+        <Screen>
+            <FlatList
+                data={messages} 
+                keyExtractor={(message) => message.id.toString()}
+                renderItem={({ item }) => (
+                    <ListItem
+                        title={item.title}
+                        subTitle={item.description}
+                        image={item.image}
+                        onPress={() => console.log("Message selected", item)}
+                        renderRightActions={() => (
+                            <ListItemDeleteAction onPress={() => handleDelete(item)} />
+                        )}
+                    />
+                )}
+                ItemSeparatorComponent={ListItemSeparator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 2,
+                            title: "T2",
+                            description: "D2",
+                            image: require("../assets/user.jpg"),
+                        },
+                    ]);
+                }}
+            />
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
