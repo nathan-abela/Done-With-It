e11694afd4e655e3ec3ae7a40c069ac885ebf1e6
Nathan Abela <nathanabela7@gmail.com>
2020-06-16 19:30:31 +0200

    Add Icon Component

---
+++ app/components/Icon.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function Icon({
+	name,
+	size = 40,
+	backgroundColor = "#000",
+	iconColor = "#fff",
+}) 
+{
+	return (
+		<View
+			style={{
+				width: size,
+				height: size,
+				borderRadius: size / 2,
+				backgroundColor,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<MaterialCommunityIcons
+				name={name}
+				color={iconColor}
+				size={size * 0.5}
+			/>
+		</View>
+	);
+}
+
+export default Icon;
