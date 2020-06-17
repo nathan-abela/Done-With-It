b61f7975599c6fff03304bf810f76860550aaf88
Nathan Abela <nathanabela7@gmail.com>
2020-06-17 18:35:07 +0200

    Add Listings Screen

---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+import Card from "../components/Card";
+import Screen from "../components/Screen";
+
+import colors from "../config/colors";
+
+const listings = [
+    {
+        id: 1,
+        title: "Red Jacket for Sale",
+        price: 100,
+        image: require("../assets/jacket.jpg"),
+    },
+    {
+        id: 2,
+        title: "Couch in Great Condition",
+        price: 1000,
+        image: require("../assets/couch.jpg"),
+    },
+];
+
+function ListingsScreen(props) {
+    return (
+        <Screen style={styles.screen}>
+            <FlatList
+                data={listings}
+                keyExtractor={(listing) => listing.id.toString()}
+                renderItem={({ item }) => (
+                    <Card
+                        title={item.title}
+                        subTitle={"€" + item.price}
+                        image={item.image}
+                    />
+                )}
+            />
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        padding: 20,
+        backgroundColor: colors.light,
+    },
+});
+
+export default ListingsScreen;
