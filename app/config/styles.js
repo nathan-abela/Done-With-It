18003d12d38180afa3474bd87ec70a6e1f9ff345
Nathan Abela <nathanabela7@gmail.com>
2020-06-18 21:59:09 +0200

    Add Styles Config

---
+++ app/config/styles.js
@@ -0,0 +1,12 @@
+import { Platform } from "react-native";
+
+import colors from "./colors";
+
+export default {
+    colors,
+    text: {
+        color: colors.dark,
+        fontSize: 18,
+        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    },
+};
