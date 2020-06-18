30f52c485d9afb4f1a56ea14d23aa13945621540
Nathan Abela <nathanabela7@gmail.com>
2020-06-18 22:00:22 +0200

    Update App Text Component

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-    return <Text style={[styles.text, style]}>{children}</Text>;
+    return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
-
-const styles = StyleSheet.create({
-    text: {
-        fontSize: 18,
-        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    },
-});
 
 export default AppText;
