907eba6d43fa6c3826b7272d90d9023d6a26f34f
Nathan Abela <nathanabela7@gmail.com>
2020-06-19 17:58:27 +0200

    Add Login Screen

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import React from "react";
+import * as Yup from "yup";
+import { StyleSheet, Image } from "react-native";
+
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms"
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password")
+});
+
+function LoginScreen(props) {
+    return (
+        <Screen style={styles.container}>
+            <Image
+                style={styles.logo}
+                source={require("../assets/logo-red.png")}
+            />
+
+            <AppForm
+                initialValues={{ email: "", password: "" }}
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+            >
+                <AppFormField
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    icon="email"
+                    name="email"
+                    placeholder="Email"
+                    textContentType="emailAddress"
+                    keyboardType="email-address"
+                />
+
+                <AppFormField
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    icon="lock"
+                    name="password"
+                    placeholder="Password"
+                    textContentType="password"
+                    secureTextEntry={true}
+                />
+                <SubmitButton title="Login" />
+            </AppForm>
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    logo: {
+        width: 80,
+        height: 80,
+        alignSelf: "center",
+        marginTop: 50,
+        marginBottom: 20,
+    },
+})
+
+export default LoginScreen;
