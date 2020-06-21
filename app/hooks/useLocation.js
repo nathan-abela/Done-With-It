c3942c578cea2b05c60deaedb76041d2d2a7dcd8
Nathan Abela <nathanabela7@gmail.com>
2020-06-21 11:48:52 +0200

    Add Use Location Hook

---
+++ app/hooks/useLocation.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+export default useLocation = () => {
+    const [location, setLocation] = useState();
+
+    const getLocation = async () => {
+        try {
+            const { granted } = await Location.requestPermissionsAsync();
+            if (!granted) return;
+            const {
+                coords: { latitude, longitude },
+            } = await Location.getLastKnownPositionAsync();
+                setLocation({ latitude, longitude });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getLocation();
+    }, []);
+
+    return location;
+};
