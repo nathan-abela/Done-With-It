282640aa8227bca48a6bab8acf79fde3c24c588b
Nathan Abela <nathanabela7@gmail.com>
2020-06-21 11:49:42 +0200

    Update Listing Edit Screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -2,15 +2,17 @@ import React from "react";
 import * as Yup from "yup";
 import { StyleSheet } from "react-native";
 
-import { Form, FormField, FormPicker, SubmitButton } from "../components/forms";
-import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import { Form, FormField, FormPicker, FormImagePicker, SubmitButton } from "../components/forms";
+import Screen from "../components/Screen";
+import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
     title: Yup.string().required().min(1).label("Title"),
     price: Yup.number().required().min(1).max(10000).label("Price"),
     description: Yup.string().label("Description"),
     category: Yup.object().required().nullable().label("Category"),
+    images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
@@ -71,6 +73,8 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+    const location = useLocation();
+
     return (
         <Screen style={styles.container}>
             <Form
@@ -79,10 +83,13 @@ function ListingEditScreen() {
                     price: "",
                     description: "",
                     category: null,
+                    images: [],
                 }}
-                onSubmit={(values) => console.log(values)}
+                onSubmit={(values) => console.log(location)}
                 validationSchema={validationSchema}
             >
+                <FormImagePicker name="images" />
+
                 <FormField maxLength={255} name="title" placeholder="Title" />
 
                 <FormField
