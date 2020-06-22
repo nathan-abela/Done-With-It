1492de0920ae261619270091fbb224487e043084
Nathan Abela <nathanabela7@gmail.com>
2020-06-22 19:55:13 +0200

    Update Form Image Picker Component

--- app/components/forms/FormImagePicker.js
+++ app/components/forms/FormImagePicker.js
@@ -5,36 +5,31 @@ import ErrorMessage from "./ErrorMessage";
 import ImageInputList from "../ImageInputList";
 
 function FormImagePicker({ name }) {
-    const { errors, setFieldValue, touched, values } = useFormikContext();
-    const imageUris = values[name]
+	const { errors, setFieldValue, touched, values } = useFormikContext();
+	const imageUris = values[name];
 
-    const handleAdd = (uri) => {
-        setFieldValue(name, [...imageUris, uri])
-    }
-    
-      const handleRemove = (uri) => {
-        setFieldValue(
-            name,
-            imageUris.filter((imageUir) => imageUir !== uri),
-        )
-    }
-    
+	const handleAdd = (uri) => {
+		setFieldValue(name, [...imageUris, uri]);
+	};
 
-    return (
-        <>
-            <ImageInputList
-                imageUris={imageUris}
-                onAddImage={handleAdd}
-                onRemoveImage={handleRemove}
-            />
+	const handleRemove = (uri) => {
+		setFieldValue(
+			name,
+			imageUris.filter((imageUir) => imageUir !== uri)
+		);
+	};
 
-            <ErrorMessage
-                error={errors[name]}
-                visibility={touched[name]}
-            />
-        </>
-    );
+	return (
+		<>
+			<ImageInputList
+				imageUris={imageUris}
+				onAddImage={handleAdd}
+				onRemoveImage={handleRemove}
+			/>
+
+			<ErrorMessage error={errors[name]} visibility={touched[name]} />
+		</>
+	);
 }
-
 
 export default FormImagePicker;
