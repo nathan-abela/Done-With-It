77782e5322d4eab1b1d4c30a4bd9c323c3941e58
Nathan Abela <nathanabela7@gmail.com>
2020-06-23 22:12:42 +0200

    Update Configs Formatting

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,10 +1,10 @@
 export default {
-    primary: "#fc5c65",
-    secondary: "#4ecdc4",
-    black: "#000",
-    white: "#fff",
-    medium: "#6e6969",
-    light: "#f8f4f4",
-    dark: "#0c0c0c",
-    danger: "#ff5252"
-}
+	primary: "#fc5c65",
+	secondary: "#4ecdc4",
+	black: "#000",
+	white: "#fff",
+	medium: "#6e6969",
+	light: "#f8f4f4",
+	dark: "#0c0c0c",
+	danger: "#ff5252",
+};
--- app/config/styles.js
+++ app/config/styles.js
@@ -3,10 +3,10 @@ import { Platform } from "react-native";
 import colors from "./colors";
 
 export default {
-    colors,
-    text: {
-        color: colors.dark,
-        fontSize: 18,
-        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-    },
+	colors,
+	text: {
+		color: colors.dark,
+		fontSize: 18,
+		fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+	},
 };
