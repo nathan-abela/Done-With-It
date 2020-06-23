ff3bdfb57ee3b92f5906c02cf63a10923f275402
Nathan Abela <nathanabela7@gmail.com>
2020-06-23 22:13:43 +0200

    Update Use Location Hook Formatting

--- app/hooks/useLocation.js
+++ app/hooks/useLocation.js
@@ -2,24 +2,24 @@ import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 export default useLocation = () => {
-    const [location, setLocation] = useState();
+	const [location, setLocation] = useState();
 
-    const getLocation = async () => {
-        try {
-            const { granted } = await Location.requestPermissionsAsync();
-            if (!granted) return;
-            const {
-                coords: { latitude, longitude },
-            } = await Location.getLastKnownPositionAsync();
-                setLocation({ latitude, longitude });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const getLocation = async () => {
+		try {
+			const { granted } = await Location.requestPermissionsAsync();
+			if (!granted) return;
+			const {
+				coords: { latitude, longitude },
+			} = await Location.getLastKnownPositionAsync();
+			setLocation({ latitude, longitude });
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    useEffect(() => {
-        getLocation();
-    }, []);
+	useEffect(() => {
+		getLocation();
+	}, []);
 
-    return location;
+	return location;
 };
