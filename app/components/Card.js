c3ff85185b388956c4fccc7f655bb334e0e3983d
Nathan Abela <nathanabela7@gmail.com>
2020-06-25 23:10:01 +0200

    Update Card Component

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,20 +1,31 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Image,
+	TouchableWithoutFeedback,
+} from "react-native";
 
 import Text from "./Text";
 
 import colors from "../config/colors";
 
-function Card({ title, subTitle, image }) {
+function Card({ title, subTitle, image, onPress }) {
 	return (
-		<View style={styles.card}>
-			<Image style={styles.image} source={image} />
+		<TouchableWithoutFeedback onPress={onPress}>
+			<View style={styles.card}>
+				<Image style={styles.image} source={image} />
 
-			<View style={styles.detailsContainer}>
-				<Text style={styles.title}>{title}</Text>
-				<Text style={styles.subTitle}>{subTitle}</Text>
+				<View style={styles.detailsContainer}>
+					<Text style={styles.title} numberOfLines={1}>
+						{title}
+					</Text>
+					<Text style={styles.subTitle} numberOfLines={2}>
+						{subTitle}
+					</Text>
+				</View>
 			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
