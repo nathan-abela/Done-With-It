38304c4d6679e2200a62d8c0dddf95e3d456e4b1
Nathan Abela <nathanabela7@gmail.com>
2020-06-27 19:08:44 +0200

    Fix Listing Details Screen navigation issue

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 const FeedNavigator = () => (
 	<Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
 		<Stack.Screen name="Listings" component={ListingsScreen} />
-		<Stack.Screen name="ListingDetail" component={ListingDetailsScreen} />
+		<Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
 	</Stack.Navigator>
 );
 
